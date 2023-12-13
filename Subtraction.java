344f98f0f3dc3967234312859969e395b85ea15f
govindraj-7c <govindrajchavan7@gmail.com>
2023-12-13 16:19:20 +0530

    subtraction program

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Subtraction of 2 number is : "+(a-b));
+    }
+}
