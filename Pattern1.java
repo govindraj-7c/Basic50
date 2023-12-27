5048ad04de296d563145e633b5ea8eae1c5f4c15
govindraj-7c <govindrajchavan7@gmail.com>
2023-12-27 10:16:07 +0530

    pattern1

---
+++ Pattern1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int no = sc.nextInt();
+        for(int i=0; i<no; i++){
+            for(int j=0; j<no; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
