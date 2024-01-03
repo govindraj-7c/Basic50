ce62fe5f66b65306183c15089b23e0bdad366fa2
govindraj-7c <govindrajchavan7@gmail.com>
2024-01-03 10:45:51 +0530

    pattern5

---
+++ Pattern5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No:");
+        int no = sc.nextInt();
+        for(int i=0; i<no; i++){
+            for(int j=no-i; j>1; j--){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
