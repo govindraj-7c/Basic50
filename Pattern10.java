5ed1b3fd7e897f6f52f42b02f4e8e260c50e33e6
govindraj-7c <govindrajchavan7@gmail.com>
2024-01-07 10:47:37 +0530

    pattern10

---
+++ Pattern10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int no = sc.nextInt();
+        for(int i=1; i<=no; i++){
+            for(int j=1; j<=no; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
